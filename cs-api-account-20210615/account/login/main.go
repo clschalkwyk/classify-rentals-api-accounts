16f3ac63e1910c9540b32e98ab8915db05ab74e4
wikusSchalkwyk <wikus@hyvemobile.co.za>
2021-07-10 23:44:34 +0200

    init

--- cs-api-account-20210615/account/login/main.go
+++ cs-api-account-20210615/account/login/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
@@ -13,8 +16,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/mergermarket/go-pkcs7"
+	_ "github.com/mergermarket/go-pkcs7"
 	"golang.org/x/crypto/pbkdf2"
+	"io"
 	"os"
+	"time"
 )
 
 type LoginRequestDto struct {
@@ -32,6 +39,18 @@ type UserDto struct {
 	OwnerId  string
 }
 
+type SessionTokenDto struct {
+	OwnerId string `json:"owner_id"`
+	Email   string `json:"email"`
+	Exp     int64  `json:"exp"`
+}
+
+var sharedKey = []byte("s3ccrethatmaycontainch@r$32chars")
+
+const CIPHER_KEY = "s3ccrethatmaycontainch@r$32chars"
+
+var iv = []byte{15, 42, 27, 34, 84, 45, 54, 34, 86, 45, 84, 98, 63, 35, 16, 3}
+
 // Response is of type APIGatewayProxyResponse since we're leveraging the
 // AWS Lambda Proxy Request functionality (default behavior)
 //
@@ -47,9 +66,6 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Respon
 	if err != nil {
 		return Response{StatusCode: 500}, err
 	}
-
-	fmt.Println(loginRequest)
-
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
@@ -90,22 +106,39 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Respon
 	}
 
 	if len(existingUser.Items) == 1 {
-		fmt.Println("User Found")
-		fmt.Println(existingUser.Items)
 		user := UserDto{}
 		err = dynamodbattribute.UnmarshalMap(existingUser.Items[0], &user)
 		if err != nil {
 			return Response{StatusCode: 500}, nil
 		}
-		fmt.Println(user)
+
 		salt := user.Password[0:66]
 		encoded := user.Password[66:len(user.Password)]
 
 		temp := pbkdf2.Key([]byte(loginRequest.Password), []byte(salt), 10000, 50, sha256.New)
 		if encoded == fmt.Sprintf("%x", temp) {
 			fmt.Println("Password Match")
+
+			sessionToken := SessionTokenDto{
+				OwnerId: user.OwnerId,
+				Email:   user.Email,
+				Exp:     time.Now().Add(time.Minute * 15).Unix(),
+			}
+
+			sessionTokenMarshalled, err := json.Marshal(&sessionToken)
+			if err != nil {
+				fmt.Println(err)
+				return Response{StatusCode: 500}, err
+			}
+
+			//encryptedSessionToken := Encrypt("a=s@eiyu$#wp*a=sa=s@eiyu$#wp*a=s", string(sessionTokenMarshalled))
+			encryptedSessionToken, err := Encrypt2(string(sessionTokenMarshalled))
+			if err != nil {
+				return Response{StatusCode: 500}, err
+			}
+
 			body, err := json.Marshal(map[string]interface{}{
-				"token": base64.StdEncoding.EncodeToString([]byte("abc-654")),
+				"token": encryptedSessionToken,
 			})
 
 			if err != nil {
@@ -132,3 +165,72 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Respon
 func main() {
 	lambda.Start(Handler)
 }
+
+func encodeBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func decodeBase64(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func Encrypt(key, text string) string {
+	fmt.Println("Encrypting: ", text)
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+	plaintext := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	ciphertext := make([]byte, len(plaintext))
+	cfb.XORKeyStream(ciphertext, plaintext)
+
+	return encodeBase64(ciphertext)
+}
+
+func Encrypt2(unencrypted string) (string, error) {
+	key := []byte(CIPHER_KEY)
+	plainText := []byte(unencrypted)
+	plainText, err := pkcs7.Pad(plainText, aes.BlockSize)
+	if err != nil {
+		return "", fmt.Errorf(`plainText: %s has error`, plainText)
+	}
+	if len(plainText)%aes.BlockSize != 0 {
+		return "", fmt.Errorf(`plainText: %s invalid block size`, plainText)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
+
+	return fmt.Sprintf("%x", cipherText), nil
+
+}
+
+func Decrypt(key, text string) string {
+	fmt.Println(text)
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+	ciphertext := decodeBase64(text)
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	plaintext := make([]byte, len(ciphertext))
+	cfb.XORKeyStream(plaintext, ciphertext)
+
+	return string(plaintext)
+}
+
+// {"owner_id":"3c4072f167a8480e8bc27f3217ee7729","email":"hello@cyberstaffing.co.za","expires":"2021-06-19 18:15:53.8708928 +0000 UTC"}
